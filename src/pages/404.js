1673939f01813a40230934b5073fe28be2516f4b
r2abreu <r2abreu@pm.me>
2020-11-13 13:22:04 +0100

    Created Navbar with Gatsby Link component

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<p>That page doesn't exist, so go back.</p>
+		</div>
+	);
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<React.Fragment>
+			<nav>
+				<Link to="/blog/">Blog</Link>
+				<Link to="https://www.gatsbyjs.org" target="_blank">
+					Gatsby Website
+				</Link>
+			</nav>
+			<h1>This is the Home Page</h1>
+		</React.Fragment>
+	);
 }
